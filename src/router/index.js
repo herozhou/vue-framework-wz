0aa8da2f3f9596769f3d09f02a1fbce4c32eafdb
zhouhero <zhouhero@hotmail.com>
2017-09-03 15:23:08 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -78,7 +78,6 @@ export default new Router({
 });
 
 export const asyncRouterMap = [
-//     component: Layout,
 //     redirect: '/permission/index',
 //     name: '权限测试',
 //     icon: 'quanxian',
