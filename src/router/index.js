7842f0848cb8d83b65653380261fc2a03646fdf9
zhouhero <zhouhero@hotmail.com>
2017-09-03 15:22:04 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -160,7 +160,6 @@ export const asyncRouterMap = [
           icon:"social-html5",
           component: wz
         },
-       
           ]
     },
     { path: '*', redirect: '/pages/404', hidden: true }
