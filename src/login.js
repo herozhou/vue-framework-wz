132dba3702f3cb6c2dc7e7cfe54b2bc833731ece
zhouhero <zhouhero@hotmail.com>
2017-10-01 22:59:34 +0800

    fixed bugs

--- src/login.js
+++ src/login.js
@@ -34,7 +34,7 @@ router.beforeEach((to, from, next) => {
           })
         })
       } else {
-        // 没有动态改变权限的需求可直接next() 删除下方权限判断 ↓
+        // () 删除下方权限判断 ↓
         store.dispatch('getNowRoutes', to);
 
         if (hasPermission(store.getters.roles, to.meta.role)) {
